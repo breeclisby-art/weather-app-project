61ed3761c92a739124a0c5296ac4dbb29f1d6004
breeclisby-art <breeclisby@gmail.com>
2025-11-22 14:11:00 +0000

    Updated current time, humidity, conditions, wind speed to functional.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 function refreshWeather(response) {
   let currentTemperature = document.querySelector("#current-temperature");
-  let temperature = response.data.temperature.current;
   let weatherAppCity = document.querySelector("#weather-app-city");
+  let weatherAppCondition = document.querySelector("#weather-app-condition");
+  let humidityValue = document.querySelector("#humidity-value");
+  let windSpeedValue = document.querySelector("#wind-speed-value");
+  let weatherAppTime = document.querySelector("#weather-app-time");
+  let date = new Date(response.data.time * 1000);
 
-  currentTemperature.innerHTML = Math.round(temperature);
+  console.log(response.data);
+
+  currentTemperature.innerHTML = Math.round(response.data.temperature.current);
+  humidityValue.innerHTML = response.data.temperature.humidity;
   weatherAppCity.innerHTML = response.data.city;
+  weatherAppCondition.innerHTML = response.data.condition.description;
+  weatherAppTime.innerHTML = formatDate(date);
+  windSpeedValue.innerHTML = response.data.wind.speed;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
