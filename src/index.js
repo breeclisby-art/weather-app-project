ea1b96d48064ca4f5ff32eb5a134f85f3a155788
breeclisby-art <breeclisby@gmail.com>
2025-11-22 13:04:40 +0000

    Improved search city to formatted version, added temperature update, SheCodes weather API functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function refreshWeather(response) {
+  let currentTemperature = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let weatherAppCity = document.querySelector("#weather-app-city");
+
+  currentTemperature.innerHTML = Math.round(temperature);
+  weatherAppCity.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "21fcd3o9edfa304e81019t7faa2b944f";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function processCitySubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#enter-city-input");
-  let weatherAppCity = document.querySelector("#weather-app-city");
-  weatherAppCity.innerHTML = cityInput.value;
+
+  searchCity(cityInput.value);
 }
 
 let searchCityForm = document.querySelector("#search-city-form");
 searchCityForm.addEventListener("submit", processCitySubmit);
+
+searchCity("Adelaide");
